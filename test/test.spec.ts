aff6d4ee5c33aaae8fc3bea4a58b9adfb4d41737
Mark Greenwood <mark.greenwood@hatchdata.com>
2020-09-16 16:24:16 -0700

    Start test file for CustomerNameComparator

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,29 @@
-const { expect } = require("chai");
+/* eslint-disable @typescript-eslint/no-unused-vars,class-methods-use-this,max-classes-per-file */
+import { v4 } from "uuid";
 
-describe("This module", () => {
-  it("should do something", () => {
-    expect(true).to.equal(true);
-  });
+class Customer {
+  id: string;
+
+  firstname: string;
+
+  lastname: string;
+
+  constructor(firstname: string, lastname: string) {
+    this.id = v4();
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+
+class CustomerNameComparator {
+  compare(c1: Customer, c2: Customer): number {
+    // TODO: implement comparator
+    return 0;
+  }
+}
+
+describe("CustomerNameComparator", () => {
+  it(
+    "should return negative number when lastname 1 is alphabetically before lastname 2",
+  );
 });
